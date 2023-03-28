c439e426dc6cf8b4062e299ebb29c7c139255b69
Michał Szajbe <michal.szajbe@gmail.com>
2023-03-28 12:42:24 +0200

    Relax elixir requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pogo.MixProject do
     [
       app: :pogo,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
