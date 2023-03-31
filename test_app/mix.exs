a731b9420bf0f8459c338d2d5871c4d0d3bbf8d7
Michał Szajbe <michal.szajbe@gmail.com>
2023-03-31 14:24:26 +0200

    Require elixir ~> 1.11

--- test_app/mix.exs
+++ test_app/mix.exs
@@ -5,7 +5,7 @@ defmodule TestApp.MixProject do
     [
       app: :test_app,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
