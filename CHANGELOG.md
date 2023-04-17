49d715ef99472166bc57aacaa06f048704c33c75
Michał Szajbe <michal.szajbe@gmail.com>
2023-04-17 11:25:58 +0200

    Bump version to 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.2.0
+
+* Rework internal process lifecycle to improve reliability of process termination.
+
 ## 0.1.0
 
 Initial release.
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pogo.MixProject do
   def project do
     [
       app: :pogo,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
